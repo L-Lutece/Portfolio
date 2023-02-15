f031d83e289ec554a54dacf1d34dc4ee04d1d17a
Lutece <datsik771@outlook.com>
2023-02-15 15:51:14 -0300

    Update Emial Adress and link

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,7 +7,7 @@ function SideBar() {
     return (
         <div className='containerSideBar'>
             <div className='SideBar'>
-                <a href='https://www.linkedin.com/in/lucas-rodrigues-830636202/'><BsLinkedin size={35}/></a>
+                <a href='https://www.linkedin.com/in/lutece771/'><BsLinkedin size={35}/></a>
                 <a href='https://github.com/L-Lutece'><BsGithub size={35}/></a>
                 <a href='https://1drv.ms/w/s!AoeTPjBJhtYXhu168u4SQvRYfr85Ww?e=4TV4IH' title="Currículo"><FaClipboardList size={35}/></a>
             </div>
