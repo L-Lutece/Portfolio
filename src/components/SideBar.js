f63f3adaf2f587bca29daeaca3e41a96f293b430
Lutece <datsik771@outlook.com>
2023-02-16 17:35:19 -0300

    improved sideBar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './SideBar.css';
+import myFile from "../images/CVLucasCarrilho.pdf";
 import { BsLinkedin, BsGithub} from "react-icons/bs";
 import { FaClipboardList } from "react-icons/fa";
 
@@ -9,7 +10,7 @@ function SideBar() {
             <div className='SideBar'>
                 <a href='https://www.linkedin.com/in/lutece771/'><BsLinkedin size={35}/></a>
                 <a href='https://github.com/L-Lutece'><BsGithub size={35}/></a>
-                <a href='https://1drv.ms/w/s!AoeTPjBJhtYXhu168u4SQvRYfr85Ww?e=4TV4IH' title="Currículo"><FaClipboardList size={35}/></a>
+                <a href={myFile} download title="Currículo"><FaClipboardList size={35}/></a>
             </div>
         </div>
     )
